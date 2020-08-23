505ab07c4335092b0259d2d41e6858f9944813ab
Nachiket Patel <patel.nachiket.r@gmail.com>
2020-08-23 08:53:09 +0530

    added env & message

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var http = require('http')
+var os = require("os");
 
-var port = 4000
+var port = process.env.PORT || 8080;
+var message = process.env.MESSAGE || "Hello world!";
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World\n')
+  var msg = JSON.stringify({
+      message: message,
+      platform: os.type(),
+      release: os.release(),
+      hostName: os.hostname()
+  })
+  response.end(msg+'\n')
 })
 
 server.listen(port)
